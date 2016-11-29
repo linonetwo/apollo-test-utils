8bd26d04645b6eb506964bb9db6651371dec5d21
lin onetwo <linonetwo012@gmail.com>
2016-11-29 14:22:27 +0800

    feat: Add two tests to make sure project running

--- tests/index.ts
+++ tests/index.ts
@@ -1,7 +1,27 @@
 import { assert } from 'chai';
 
-describe('example', () => {
-  it('should be equal', () => {
+describe('Test', () => {
+  it('should be running', () => {
     assert.equal(true, true);
   });
 });
+
+import './QueryManager';
+// import './client';
+// import './store';
+// import './queryTransform';
+// import './getFromAST';
+// import './directives';
+// import './batching';
+// import './scheduler';
+// import './mutationResults';
+// import './optimistic';
+// import './fetchMore';
+// import './scopeQuery';
+// import './errors';
+// import './mockNetworkInterface';
+// import './graphqlSubscriptions';
+import './batchedNetworkInterface';
+// import './ObservableQuery';
+// import './subscribeToMore';
+// import './customResolvers';
